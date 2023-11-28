08e7430ba391f119142f777c5e8aedaf4981ab3c
RobStarkie <rc.starkie@yahoo.co.uk>
2023-11-28 11:12:28 +0000

    chnage password and user profile available

--- Database.py
+++ Database.py
@@ -10,7 +10,7 @@ class Database:
         database = 'hr_management'
         )
         return mydb
-  
+        
     def createTables(mydb):
         fd = open('database_create.sql', 'r')
         sqlFile = fd.read()
@@ -41,3 +41,20 @@ class Database:
             lineManager=True
         tempUser = User(int(result[0]), result[1], result[2], result[3],result[4], result[5], int(result[6]), lineManager, int(result[8]), int(result[9]))
         return tempUser
+    
+    def updateUserInfoPassword(mydb, userID, password):
+        cursor = mydb.cursor()
+        sqlCommand = "UPDATE users SET Password = '"+password+"' WHERE UserID = '"+str(userID)+"';"
+        print(sqlCommand)
+        cursor.execute(sqlCommand)
+        mydb.commit()
+
+    def updateEntireUser(mydb, user):
+        lineManager=0
+        if user.lineManager == True:
+            lineManager=1
+        sqlCommand = "UPDATE users SET Email = '"+user.email+"', FirstName = '"+user.firstName+"', SecondName= '"+user.secondName+"',  Password= '"+user.password+"',  ProfilePicture = '"+user.profilePicture+"', PhoneNumber = '"+str(user.phoneNumber)+"', LineManager = '"+str(lineManager)+"', LineManagerID = '"+str(user.lineManagerID)+"', TotalHolidays = '"+str(user.totalHolidays)+"' WHERE UserID = '"+str(user.userID)+"';"
+        print(sqlCommand)
+        cursor = mydb.cursor()
+        cursor.execute(sqlCommand)
+        mydb.commit()
--- databaseTests.py
+++ databaseTests.py
@@ -1,17 +1,15 @@
 from Database import *
 from User import *
 
-def test_database_createTables():
 
+def test_database_createTables():
     mydb = Database.connectToDB()
     Database.createTables(mydb)
-    #add user to table
-    
-    #assert cursor == "('hr_management',),('information_schema',),('mysql',),('performance_schema',)"
+    assert (mydb != None )
 
 def test_createAddingAndRetrievingUserFromUserTable():
     mydb = Database.connectToDB()
-    testUser = User(1,"test@email.com", "Rob", "Stark","password", "picture", 12333446, False, 1278654, 25 )
+    testUser = User(1,"test@email.com", "Rob", "Stark","das", "picture", 12333446, False, 1278654, 25 )
     Database.addNewUserToDB(mydb, testUser)
     result = Database.getUserFromUserTable(mydb, 1)
     assert (testUser.userID ==result.userID)
@@ -24,4 +22,42 @@ def test_createAddingAndRetrievingUserFromUserTable():
     assert (testUser.lineManager ==result.lineManager)
     assert (testUser.lineManagerID ==result.lineManagerID)
     assert (testUser.totalHolidays ==result.totalHolidays)
+
+def test_changeUserPassword():
+    mydb = Database.connectToDB()
+    newPassword = "hagen"
+    testUser = User(2,"mattsTest@email.com", "Matt", "Connolly", "password", "picture", 12333446, False, 1278654, 25 )
+    Database.addNewUserToDB(mydb, testUser)
+    Database.updateUserInfoPassword(mydb, 2, newPassword)
+    result = Database.getUserFromUserTable(mydb, 2)
+    assert (testUser.userID ==result.userID)
+    assert (testUser.email ==result.email)
+    assert (testUser.firstName ==result.firstName)
+    assert (testUser.secondName ==result.secondName)
+    assert (newPassword == result.password)
+    assert (testUser.profilePicture ==result.profilePicture)
+    assert (testUser.phoneNumber ==result.phoneNumber)
+    assert (testUser.lineManager ==result.lineManager)
+    assert (testUser.lineManagerID ==result.lineManagerID)
+    assert (testUser.totalHolidays ==result.totalHolidays)
+
+def test_changeUserProfile():
+    mydb = Database.connectToDB()
+    initalProfile = User(3,"someTest@email.com", "Matt", "Connolly", "password", "picture", 12333446, False, 1278654, 25 )
+    newProfile  = User(3,"donkey@email.com", "Donkey", "Kong", "mario", "luigi", 555555, False, 555555, 35 )
+    Database.addNewUserToDB(mydb, initalProfile)
+    Database.updateEntireUser(mydb, newProfile)
+    result = Database.getUserFromUserTable(mydb, 3)
+    assert (newProfile.userID ==result.userID)
+    assert (newProfile.email ==result.email)
+    assert (newProfile.firstName ==result.firstName)
+    assert (newProfile.secondName ==result.secondName)
+    assert (newProfile.password == result.password)
+    assert (newProfile.profilePicture ==result.profilePicture)
+    assert (newProfile.phoneNumber ==result.phoneNumber)
+    assert (newProfile.lineManager ==result.lineManager)
+    assert (newProfile.lineManagerID ==result.lineManagerID)
+    assert (newProfile.totalHolidays ==result.totalHolidays)
+
+
 
