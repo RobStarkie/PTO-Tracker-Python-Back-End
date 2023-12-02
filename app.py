a5022e5497bbd3471bc9cda2b33bf917066caf95
Matt Connolly <54942633+matty64047@users.noreply.github.com>
2023-12-02 06:57:13 +0000

    bank holidays

--- app.py
+++ app.py
@@ -49,3 +49,34 @@ def logout():
     response = jsonify({"msg": "logout successful"})
     unset_jwt_cookies(response)
     return response
+
+
+from datetime import datetime, timedelta
+from govuk_bank_holidays.bank_holidays import BankHolidays
+
+def count_work_hours(start_date, end_date, work_hours_per_day, total_holiday_hours):
+    bank_holidays = BankHolidays()
+    holidays = {holiday['date'] for holiday in bank_holidays.get_holidays()}
+
+    current_date = start_date
+    total_work_hours = 0
+
+    while current_date <= end_date:
+        # Check if the current day is a weekday and not a bank holiday
+        if current_date.weekday() < 5 and current_date not in holidays:
+            total_work_hours += work_hours_per_day[current_date.weekday()]
+
+        # Move to the next day
+        current_date += timedelta(days=1)
+
+    return total_holiday_hours - total_work_hours
+
+"""# Example usage:
+start_date = datetime(2023, 1, 1)  # Replace with your start date
+end_date = datetime(2023, 12, 31)   # Replace with your end date
+work_hours_per_day = [8, 8, 8, 8, 5]  # Replace with your work hours for each day
+total_holiday_hours = 137
+
+total_work_hours = count_work_hours(start_date, end_date, work_hours_per_day, total_holiday_hours)
+print(f"Holiday hours remaining {start_date.date()} and {end_date.date()}: {total_work_hours}")
+"""
