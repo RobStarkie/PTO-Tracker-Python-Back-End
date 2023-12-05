786f925b4862c1146483f88815c92349557ef42c
Ryan Salisbury <ryansalisbury10@icloud.com>
2023-12-05 15:33:07 +0000

    Correctly logs in users from DB, integration

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from Database import *
+from Database import Database
 import json
-from flask import Flask, request, jsonify, Response
+from flask import Flask, request, jsonify
 from datetime import datetime, timedelta, timezone
 from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
                                unset_jwt_cookies, jwt_required, JWTManager
 from flask_cors import CORS, cross_origin
-from Service import *
-import json
+
+mydb = Database.connectToDB()
+Database.createTables(mydb)
 
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = "please-remember-to-change-me"
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=2)
 CORS(app)
 jwt = JWTManager(app)
 
@@ -36,73 +38,28 @@ def refresh_expiring_jwts(response):
 def create_token():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
-    try:
-        if Service.login(email, password)==False:
-            return {"msg": "Wrong email or password"}, 401
-    except TypeError:
+    userId = Database.get_user_id_from_email(mydb, email)
+    print(userId)
+
+    if userId is None:
+        return {"msg": "User not found"}, 404
+    user = Database.getUserFromUserTableForLogin(mydb, userId, password)
+    print(user)
+    if isinstance(user, dict):
+        return user, 401
+
+    if user:
+        access_token = create_access_token(identity=userId)
+        return {"access_token": access_token}
+    else:
         return {"msg": "Wrong email or password"}, 401
-    access_token = create_access_token(identity=email)
-    response = {"access_token":access_token}
-    return response
+
 
 @app.route("/logout", methods=["POST"])
 def logout():
-    current_user = get_jwt_identity()
-    print(logged_in_as=current_user)
     response = jsonify({"msg": "logout successful"})
     unset_jwt_cookies(response)
     return response
-
-
-
-@app.route("/getHolidays", methods =["GET"])
-@jwt_required()
-def getHolidays():
-    print("getting holidays")
-    current_user = get_jwt_identity()
-    print(current_user)
-    user = Service.getUserByEmail(current_user)
-    print("userID: "+str(user.userID))
-    hr = Service.getUserHolidayRequests(user.userID)
-    print("hr: ")
-    print(hr[0].requestID)
-    #hr = np.array(hr)
-    print(hr)
-    list = []
-    for x in hr:
-        a = x.userID
-        list.append(a)
-        b = x.startDate.strftime("%Y-%m-%d")
-        list.append(b)
-        c = x.endDate.strftime("%Y-%m-%d")
-        list.append(c)
-        d = x.status.name
-        list.append(d)
-    print(list)
-    return json.dumps(list)
-
-@app.route("/addNewHolidayRequest", methods =["POST"])
-@jwt_required()
-def addNewHolidayRequest():
-    
-    try:
-        startDate = request.json.get("startDate", None)
-        endDate = request.json.get("endDate", None)
-        date1 = datetime.strptime(startDate, '%Y-%m-%d').date()
-        date2 = datetime.strptime(endDate, '%Y-%m-%d').date()
-        if(date1<=date2):
-            current_user = get_jwt_identity()
-            user = Service.getUserByEmail(current_user)
-            hr = HolidayRequest(None, user.userID, startDate, endDate, Status.REVIEW)
-            Service.addNewHolidayRequest(hr)
-            return {"msg": "New Holiday Request Added"}, 200
-            return {"msg": "Date is goof"}, 200
-        else:
-            return{"msg": "Dates are incorrect"}, 500            
-    except TypeError:
-        print(TypeError)
-        return {"msg": "Couldnt add holiday request: "+ TypeError}, 401
-
 
 
 from datetime import datetime, timedelta
